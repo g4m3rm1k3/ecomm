fe145a1c9b6f5ebf26188267026ec3d53d072482
Michael McLean <g4m3rm1k3@hotmail.com>
2021-12-11 02:45:42 -0500

    sanatized and validated user signup input

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import { check, validationResult } from "express-validator";
 import usersRepo from "../../repositories/users.js";
 import signupTemplate from "../../views/admin/auth/signup.js";
 import signinTemplate from "../../views/admin/auth/signin.js";
@@ -10,23 +10,33 @@ const router = express.Router();
 router.get("/signup", (req, res) => {
   res.send(signupTemplate({ req }));
 });
-router.post("/signup", async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
+router.post(
+  "/signup",
+  [
+    check("email").trim().normalizeEmail().isEmail(),
+    check("password").trim().isLength({ min: 4, max: 20 }),
+    check("passwordConfirmation").trim().isLength({ min: 4, max: 20 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    const { email, password, passwordConfirmation } = req.body;
 
-  const existingUser = await usersRepo.getOneBy({ email });
-  if (existingUser) {
-    return res.send("Email in use");
-  }
-  if (password !== passwordConfirmation) {
-    return res.send("Passwords must match");
-  }
-  // Create a user in our user repe to represent this person
-  const user = await usersRepo.create({ email, password });
-  //Store the id of that user inside the user cookie
-  req.session.userId = user.id; //Added by cookie session
+    const existingUser = await usersRepo.getOneBy({ email });
+    if (existingUser) {
+      return res.send("Email in use");
+    }
+    if (password !== passwordConfirmation) {
+      return res.send("Passwords must match");
+    }
+    // Create a user in our user repe to represent this person
+    const user = await usersRepo.create({ email, password });
+    //Store the id of that user inside the user cookie
+    req.session.userId = user.id; //Added by cookie session
 
-  res.send("Account Created");
-});
+    res.send("Account Created");
+  }
+);
 router.get("/signout", (req, res) => {
   req.session = null;
   res.send("You are logged out");
